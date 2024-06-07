07b9b04cc84b9b45678bf2596bc3380fc1945961
MDanilAkbar <mdanilakbar.005@gmail.com>
2024-06-07 17:55:28 +0700

    Membuat edit data anggota

---
+++ resources/views/anggota/edit.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.master')
+@section('title','Edit Anggota')
+@section('heading','Edit Anggota')
+
+@section('content')
+    <div class="card">
+    <div class="card-body">
+            <form method="POST" action="/anggota/{{$ang->id}}">
+                @method('PUT')
+                @csrf
+
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">No Kartu</label>
+                    <input type="text" name="no_kartu" value="{{$ang->no_kartu}}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Nama</label>
+                    <input type="text" name="nama" value="{{$ang->nama}}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Tempat Lahir</label>
+                    <input type="text" name="tempatLahir" value="{{$ang->tempatLahir}}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Tanggal Lahir</label>
+                    <input type="date" name="tanggalLahir" value="{{$ang->tanggalLahir}}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Jenis Kelamin</label>
+                    <br>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="jenisKelamin" id="inlineRadio1" value="Laki-Laki">
+                        <label class="form-check-label" for="inlineRadio1">Laki Laki</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="jenisKelamin" id="inlineRadio2" value="Perempuan">
+                        <label class="form-check-label" for="inlineRadio2">Perempuan</label>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Alamat</label>
+                    <input type="text" name="alamat" value="{{$ang->alamat}}" class="form-control" id="exampleInputPassword1">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Edit Data</button>
+            </form>
+        </div>
+        </div>
+@endsection
