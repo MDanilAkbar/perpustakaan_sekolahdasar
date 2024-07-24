b5452508731fed623fb0815c857646534f9328a3
MDanilAkbar <mdanilakbar.005@gmail.com>
2024-07-24 22:41:57 +0700

    Menyesuaikan Menu

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -163,8 +163,8 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="../../index3.html" class="brand-link">
-            <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
+            <img src="../../dist/img/cth.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <span class="brand-text font-weight-light">PERPUSTAKAAN</span>
         </a>
 
         <!-- Sidebar -->
@@ -211,13 +211,28 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="/jurusan/" class="nav-link">
-                            <i class="nav-icon fas  fa-user"></i>
-                            <p>
-                                Transaksi
-                            </p>
+                    <a href="/transaksi/" class="nav-link">
+                    <i class="nav-icon fas fa-calendar-alt"></i>
+                    <p>
+                        Transaksi
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="/peminjaman/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Peminjaman</p>
                         </a>
                     </li>
+                    <li class="nav-item">
+                        <a href="/pengembalian/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Pengembalian</p>
+                        </a>
+                    </li>
+                    </ul>
+                </li>
                     <li class="nav-item">
                         <a href="/jurusan/" class="nav-link">
                             <i class="nav-icon fas fa-money-bill-wave"></i>
