f90a1536046b581743173461526d31420f781798
MDanilAkbar <mdanilakbar.005@gmail.com>
2024-08-10 08:18:10 +0700

    Update Halaman Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,23 +12,71 @@
 @endsection
 
 @section('content')
-    <div class="card">
+    {{--<div class="card">
         <div class="card-header">
         <h3 class="card-title">Title</h3>
 
-        <div class="card-tools">
+        <div class="card-tools"> 
             <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
             <i class="fas fa-minus"></i>
             </button>
             <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
             <i class="fas fa-times"></i>
             </button>
-        </div>
-        </div>
+        </div>--}}
+        {{--</div>
         <div class="card-body">
             nanti akan ada tampilan dashboard, mohon sabar
         </div>
-        <!-- /.card-body -->
+        <!-- /.card-body -->--}}
+<br>
+        <div id="layoutSidenav_content">
+                <main>
+                   
+                        <div class="row">
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-primary text-white mb-4">
+                                    <div class="card-body">{{--<i class=" fas fa-users">--}}</i>Data Anggota</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="/anggota/">Selengkapnya</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-info text-white mb-4">
+                                    <div class="card-body">Data Buku</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="/buku/">Selengkapnya</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-success text-white mb-4">
+                                    <div class="card-body">Peminjaman</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="/peminjaman/">Selengkapnya</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-secondary text-white mb-4">
+                                    <div class="card-body">Pengembalian</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="/pengembalian/">Selengkapnya</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+
+
 
     </div>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,7 +25,7 @@
                 <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
             </li>
             <li class="nav-item d-none d-sm-inline-block">
-                <a href="../../index3.html" class="nav-link">Home</a>
+                <a href="/" class="nav-link">Home</a>
             </li>
             <li class="nav-item d-none d-sm-inline-block">
                 <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault();
@@ -38,7 +38,7 @@
 
         <!-- Right navbar links -->
         <ul class="navbar-nav ml-auto">
-        <!-- Navbar Search -->
+        {{--<!-- Navbar Search -->
         <li class="nav-item">
             <a class="nav-link" data-widget="navbar-search" href="#" role="button">
             <i class="fas fa-search"></i>
@@ -58,10 +58,10 @@
                 </div>
             </form>
             </div>
-        </li>
+        </li>--}}
 
         <!-- Messages Dropdown Menu -->
-        <li class="nav-item dropdown">
+        {{--<li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#">
             <i class="far fa-comments"></i>
             <span class="badge badge-danger navbar-badge">3</span>
@@ -144,7 +144,7 @@
             <div class="dropdown-divider"></div>
             <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
             </div>
-        </li>
+        </li>--}}
         <li class="nav-item">
             <a class="nav-link" data-widget="fullscreen" href="#" role="button">
             <i class="fas fa-expand-arrows-alt"></i>
